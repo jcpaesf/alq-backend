d15fd96790a1303c21c4745b8ca69ed9ee1e26a2
jcpaesf <jcpaesf@gmail.com>
2020-12-06 11:07:49 -0300

    Teste

--- src/modules/appointments/infra/http/routes/Appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/Appointments.routes.ts
@@ -6,6 +6,6 @@ import AppointmentsController from '../controllers/AppointmentsController';
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
 
-appointmentsRouter.post('/', ensureAuthenticated, appointmentsController.create);
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
